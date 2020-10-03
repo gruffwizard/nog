8a5b7b5eabeb9b89a0c61c0498a8ac3ce81a71ed
Steve Poole <spoole167@googlemail.com>
2020-10-03 03:04:10 +0100

    modulise nog

--- launcher/docker.go
+++ launcher/docker.go
@@ -1,13 +1,8 @@
 package launcher
 
 import (
-  "context"
-  "github.com/docker/docker/api/types"
-  "github.com/docker/docker/api/types/container"
-  "github.com/docker/docker/client"
-  "github.com/docker/docker/pkg/stdcopy"
-  "io"
-  "os"
+  "github.com/fsouza/go-dockerclient"
+  "fmt"
 )
 
 func CheckForValidDockerVolume(vol string) (bool,error) {
@@ -19,44 +14,7 @@ func CheckForValidDockerVolume(vol string) (bool,error) {
 
 func RunContainer() {
 
-  ctx := context.Background()
-  cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-  if err != nil {
-    panic(err)
+    client, err := docker.NewClientFromEnv()
+    fmt.Println("resp:%v\n",client)
+    fmt.Println("err:%v\n",err)
   }
-  reader, err := cli.ImagePull(ctx, "docker.io/gruffwizard/qlauncher-quarkus:0.0.1", types.ImagePullOptions{})
-    if err != nil {
-      panic(err)
-    }
-    io.Copy(os.Stdout, reader)
-
-    resp, err := cli.ContainerCreate(ctx, &container.Config{
-    Image: "alpine",
-    Cmd:   []string{"echo", "hello world"},
-    Tty:   false,
-  }, nil, nil, nil, "")
-  if err != nil {
-    panic(err)
-  }
-
-  if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-  panic(err)
-  }
-
-  statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-  select {
-  case err := <-errCh:
-  if err != nil {
-    panic(err)
-  }
-  case <-statusCh:
-  }
-
-  out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-  if err != nil {
-  panic(err)
-  }
-
-  stdcopy.StdCopy(os.Stdout, os.Stderr, out)
-
-}
